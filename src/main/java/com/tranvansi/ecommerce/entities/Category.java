3fe7bde2665fbceef1ce95e174283b5bc84361a0
Tran Van Si <tranvansi6302@gmail.com>
2024-05-14 15:35:35 +0700

    feature-create-category

---
+++ src/main/java/com/tranvansi/ecommerce/entities/Category.java
@@ -0,0 +1,20 @@
+package com.tranvansi.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
