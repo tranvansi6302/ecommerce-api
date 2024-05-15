570804545709c5d6f62362ae5259cbd0e6e96317
Tran Van Si <tranvansi6302@gmail.com>
2024-05-15 12:20:11 +0700

    feature-get-all-categories

--- src/main/java/com/tranvansi/ecommerce/controllers/CategoryController.java
+++ src/main/java/com/tranvansi/ecommerce/controllers/CategoryController.java
@@ -1,23 +1,48 @@
 package com.tranvansi.ecommerce.controllers;
 
 import com.tranvansi.ecommerce.dtos.requests.CreateCategoryRequest;
-import com.tranvansi.ecommerce.dtos.responses.ApiResponse;
-import com.tranvansi.ecommerce.dtos.responses.CategoryResponse;
+import com.tranvansi.ecommerce.dtos.responses.*;
 import com.tranvansi.ecommerce.enums.Message;
 import com.tranvansi.ecommerce.services.ICategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("${api.prefix}/categories")
 @RequiredArgsConstructor
 public class CategoryController {
     private final ICategoryService categoryService;
+
+
+    @GetMapping("")
+    public ResponseEntity<PagedResponse<List<CategoryResponse>>> getAllCategories(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "15") int limit,
+            @RequestParam(defaultValue = "desc") String sort_direction
+    ) {
+        Sort sort = Sort.by("createdAt");
+        sort = sort_direction.equalsIgnoreCase("asc")
+                ? sort.ascending() : sort.descending();
+        PageRequest pageRequest = PageRequest.of(page - 1, limit, sort);
+        Page<CategoryResponse> categoryResponses = categoryService.getAllCategories(pageRequest);
+        PagedResponse<List<CategoryResponse>> response = PagedResponse.<List<CategoryResponse>>builder()
+                .result(categoryResponses.getContent())
+                .pagination(PaginationResponse.builder()
+                        .page(page)
+                        .limit(limit)
+                        .totalPage(categoryResponses.getTotalPages())
+                        .build())
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
     @PostMapping("")
     public ResponseEntity<ApiResponse<CategoryResponse>> createCategory(
             @RequestBody @Valid CreateCategoryRequest request) {
@@ -29,3 +54,23 @@ public class CategoryController {
         return ResponseEntity.ok(response);
     }
 }
+
+//@GetMapping("")
+//public ResponseEntity<PagedResponse<List<CategoryResponse>>> getAllCategories(
+//        @PageableDefault(size = 15, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+//) {
+//    Page<CategoryResponse> categoryResponses = categoryService.getAllCategories(pageable);
+//    PagedResponse<List<CategoryResponse>> response = buildPagedResponse(categoryResponses, pageable);
+//    return ResponseEntity.ok(response);
+//}
+//
+//private <T> PagedResponse<List<T>> buildPagedResponse(Page<T> pageData, Pageable pageable) {
+//    return PagedResponse.<List<T>>builder()
+//            .result(pageData.getContent())
+//            .pagination(PaginationResponse.builder()
+//                    .page(pageable.getPageNumber() + 1)
+//                    .limit(pageable.getPageSize())
+//                    .totalPage(pageData.getTotalPages())
+//                    .build())
+//            .build();
+//}
--- src/main/java/com/tranvansi/ecommerce/services/ICategoryService.java
+++ src/main/java/com/tranvansi/ecommerce/services/ICategoryService.java
@@ -2,9 +2,14 @@ package com.tranvansi.ecommerce.services;
 
 import com.tranvansi.ecommerce.dtos.requests.CreateCategoryRequest;
 import com.tranvansi.ecommerce.dtos.responses.CategoryResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ICategoryService {
     @PreAuthorize("hasRole('ADMIN')")
     CategoryResponse createCategory(CreateCategoryRequest request);
+
+    @PreAuthorize("hasRole('ADMIN')")
+    Page<CategoryResponse> getAllCategories(PageRequest pageRequest);
 }
