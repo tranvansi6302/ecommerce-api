f2baea0ff727957d66612ba74b1597408148431d
Tran Van Si <tranvansi6302@gmail.com>
2024-05-15 23:31:29 +0700

    feature-update-brand

---
+++ src/main/java/com/tranvansi/ecommerce/dtos/requests/brands/UpdateBrandRequest.java
@@ -0,0 +1,21 @@
+package com.tranvansi.ecommerce.dtos.requests.brands;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateBrandRequest {
+    @NotBlank(message = "INVALID_BRAND_NAME_REQUIRED")
+    @Size(min = 3, max = 50, message = "INVALID_BRAND_SIZE")
+    private String name;
+
+    @NotBlank(message = "INVALID_BRAND_SUMMARY_REQUIRED")
+    private String summary;
+}
--- src/main/java/com/tranvansi/ecommerce/mappers/BrandMapper.java
+++ src/main/java/com/tranvansi/ecommerce/mappers/BrandMapper.java
@@ -1,12 +1,17 @@
 package com.tranvansi.ecommerce.mappers;
 
 import com.tranvansi.ecommerce.dtos.requests.brands.CreateBrandRequest;
+import com.tranvansi.ecommerce.dtos.requests.brands.UpdateBrandRequest;
 import com.tranvansi.ecommerce.dtos.responses.brans.BrandResponse;
 import com.tranvansi.ecommerce.entities.Brand;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface BrandMapper {
     Brand toBrand(CreateBrandRequest request);
+
     BrandResponse toBrandResponse(Brand brand);
+
+    void updateBrand(@MappingTarget Brand brand, UpdateBrandRequest request);
 }
