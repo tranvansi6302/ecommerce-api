472533f4b6b2b6e1c58e83c6b2b3e5921f37af65
Tran Van Si <tranvansi6302@gmail.com>
2024-05-16 15:19:22 +0700

    feature-get-all-sizes

--- src/main/java/com/tranvansi/ecommerce/controllers/SizeController.java
+++ src/main/java/com/tranvansi/ecommerce/controllers/SizeController.java
@@ -2,22 +2,42 @@ package com.tranvansi.ecommerce.controllers;
 
 import com.tranvansi.ecommerce.dtos.requests.sizes.CreateSizeRequest;
 import com.tranvansi.ecommerce.dtos.responses.common.ApiResponse;
+import com.tranvansi.ecommerce.dtos.responses.common.BuildResponse;
+import com.tranvansi.ecommerce.dtos.responses.common.PagedResponse;
 import com.tranvansi.ecommerce.dtos.responses.sizes.SizeResponse;
 import com.tranvansi.ecommerce.enums.Message;
 import com.tranvansi.ecommerce.services.sizes.ISizeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("${api.prefix}/sizes")
 @RequiredArgsConstructor
 public class SizeController {
     private final ISizeService sizeService;
+
+    @GetMapping("")
+    public ResponseEntity<PagedResponse<List<SizeResponse>>> getAllSizes(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "15") int limit,
+            @RequestParam(defaultValue = "desc") String sort_direction
+    ) {
+        Sort sort = sort_direction.equalsIgnoreCase("asc") ?
+                Sort.by("createdAt").ascending() :
+                Sort.by("createdAt").descending();
+        PageRequest pageRequest = PageRequest.of(page - 1, limit, sort);
+        Page<SizeResponse> categoryResponses = sizeService.getAllSizes(pageRequest);
+        PagedResponse<List<SizeResponse>> response = BuildResponse
+                .buildPagedResponse(categoryResponses, pageRequest);
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("")
     public ResponseEntity<ApiResponse<SizeResponse>> createSize(@RequestBody @Valid CreateSizeRequest request) {
--- src/main/java/com/tranvansi/ecommerce/services/sizes/ISizeService.java
+++ src/main/java/com/tranvansi/ecommerce/services/sizes/ISizeService.java
@@ -2,9 +2,13 @@ package com.tranvansi.ecommerce.services.sizes;
 
 import com.tranvansi.ecommerce.dtos.requests.sizes.CreateSizeRequest;
 import com.tranvansi.ecommerce.dtos.responses.sizes.SizeResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ISizeService {
     @PreAuthorize("hasRole('ADMIN')")
     SizeResponse createSize(CreateSizeRequest request);
+
+    Page<SizeResponse> getAllSizes(PageRequest pageRequest);
 }
